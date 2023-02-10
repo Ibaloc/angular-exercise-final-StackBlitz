ea7b6bdc4518164496ae17f20ee77448caa90206
Ibaloc <124224586+Ibaloc@users.noreply.github.com>
2023-02-10 16:40:47 +0800

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import {
+  Directive,
+  ElementRef,
+  forwardRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+  Self,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
 @Component({
   selector: 'my-app',
   standalone: true,
@@ -32,5 +43,52 @@ document.getElementById('lblresult').innerHTML = result.substring(
   0,
   result.length - 2
 );
+
+@Directive({
+  selector: '[appUppercase]',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => UppercaseDirective),
+      multi: true,
+    },
+  ],
+})
+export class UppercaseDirective implements ControlValueAccessor {
+  /** implements ControlValueAccessorInterface */
+  _onChange: (_: any) => void;
+
+  /** implements ControlValueAccessorInterface */
+  _touched: () => void;
+
+  constructor(@Self() private _el: ElementRef, private _renderer: Renderer2) {}
+
+  /** Trata as teclas */
+  @HostListener('keyup', ['$event'])
+  @HostListener('blur', ['$event'])
+  onBlur() {
+    this._touched();
+  }
+
+  /** Implementation for ControlValueAccessor interface */
+  writeValue(value: any): void {
+    this._renderer.setProperty(this._el.nativeElement, 'value', value);
+  }
+
+  /** Implementation for ControlValueAccessor interface */
+  registerOnChange(fn: (_: any) => void): void {
+    this._onChange = fn;
+  }
+
+  /** Implementation for ControlValueAccessor interface */
+  registerOnTouched(fn: () => void): void {
+    this._touched = fn;
+  }
+
+  /** Implementation for ControlValueAccessor interface */
+  setDisabledState(isDisabled: boolean): void {
+    this._renderer.setProperty(this._el.nativeElement, 'disabled', isDisabled);
+  }
+}
 
 bootstrapApplication(App);
